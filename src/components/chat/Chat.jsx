c57233b48d4ff3659e977b7c5b1adb49a577890b
anyars-encarta <anyarsencarta@gmail.com>
2024-04-26 12:17:38 +0000

    Implement top and bottom section of chat section

--- src/components/chat/Chat.jsx
+++ src/components/chat/Chat.jsx
@@ -3,7 +3,37 @@ import './chat.css';
 
 const Chat = () => {
   return (
-    <div className='chat-container'>Chat</div>
+    <div className='chat-container'>
+      <div className="top">
+        <div className="user">
+          <img src="./avatar.png" alt="" />
+          <div className="texts">
+            <span>Jane Doe</span>
+            <p>Lorem ipsum dolor sit amet.</p>
+          </div>
+        </div>
+        <div className="icons">
+          <img src="./phone.png" alt="" />
+          <img src="./video.png" alt="" />
+          <img src="./info.png" alt="" />
+        </div>
+      </div>
+
+      <div className="center"></div>
+
+      <div className="bottom">
+        <div className="icons">
+          <img src="./img.png" alt="" />
+          <img src="./camera.png" alt="" />
+          <img src="./mic.png" alt="" />
+        </div>
+        <input type="text" placeholder='type a message...' />
+        <div className="emoji">
+          <img src="./emoji.png" alt="" />
+        </div>
+        <button className='sendButton'>Send</button>
+      </div>
+    </div>
   )
 }
 
