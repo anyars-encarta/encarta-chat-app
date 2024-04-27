2ab54c48d5d3aad61705aab9ca77642e673dfbd1
anyars-encarta <anyarsencarta@gmail.com>
2024-04-27 13:20:48 +0000

    Implement registration and signing in

--- src/App.jsx
+++ src/App.jsx
@@ -6,25 +6,29 @@ import Login from "./components/login/Login";
 import Notification from "./components/notification/Notification";
 import { onAuthStateChanged } from "firebase/auth";
 import { auth } from "./lib/firebase";
+import { useUserStore } from "./lib/userStore";
 
 const App = () => {
-  const user = false
+  // const user = false
+  const {currentUser, isLoading, fetchUserInfo} = useUserStore();
 
   useEffect(() => {
     const unSub = onAuthStateChanged(auth, (user) => {
-      console.log(user)
-
-      
-    });
+      fetchUserInfo(user.uid);
+    }, [fetchUserInfo]);
 
     return () => {
       unSub()
     };
   }, []);
 
+  console.log(currentUser)
+
+  if(isLoading) return <div className="loading">Loading...</div>
+
   return (
     <div className='app-container'>
-      {user ? (
+      {currentUser ? (
         <>
           <List />
           <Chat />
