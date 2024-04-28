cd114414009d6d205ceecb6fa80f9ae873f31ed8
anyars-encarta <anyarsencarta@gmail.com>
2024-04-28 16:28:22 +0000

    Set backgound color of unread chats

--- src/components/list/chatList/ChatList.jsx
+++ src/components/list/chatList/ChatList.jsx
@@ -56,7 +56,12 @@ const ChatList = () => {
       </div>
 
       {chats.map((chat) => (
-        <div className="item" key={chat.chatId} onClick={() => handleSelect(chat)}>
+        <div 
+        className="item" 
+        key={chat.chatId} 
+        onClick={() => handleSelect(chat)}
+        style={{ backgroundColor: chat?.isSeen ? 'transparent' : '#5183fe' }}
+        >
           <img src={chat.user.avatar || "./avatar.png"} alt="" />
           <div className="texts">
             <span>{chat.user.username}</span>
